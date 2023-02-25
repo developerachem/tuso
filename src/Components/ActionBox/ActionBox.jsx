afec5272385568158dee671ccf4767cc69981413
developerachem <developerachem@gmail.com>
2023-02-25 15:42:35 +0600

    upload all file

---
+++ src/Components/ActionBox/ActionBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ActionBox.css'
+
+const ActionBox = ({action}) => {
+    return (
+        <div className={`list_action_box ${action ? "active" : ""}`}>
+            <p>Status</p>
+            <button> <span></span> On Hold</button>
+            <button> <span></span> Closed</button>
+        </div>
+    )
+}
+
+export default ActionBox
